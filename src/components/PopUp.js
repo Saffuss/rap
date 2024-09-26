2705f6e479d3601e05afdeb7235df3fe34c37998
Smith Fusselman <smithfuss@gmail.com>
2024-09-26 10:11:13 -0600

    Add basic function statements to components and connect components together

---
+++ src/components/PopUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Comments from "./Comments";
+
+function PopUp() {
+    return (
+        <div>
+            <h2>PopUp</h2>
+            <Comments />
+        </div>
+    )
+}
+
+export default PopUp;
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import Filters from "./Filters";
+
+function Search() {
+    return (
+        <div>
+            <h2>Search</h2>
+            <SearchBar />
+            <Filters />
+        </div>
+    )
+}
+
+export default Search;
