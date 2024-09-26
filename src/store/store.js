be1dc1954aa205ea88d0b1edc1083ae3e9d92939
Smith Fusselman <smithfuss@gmail.com>
2024-09-26 10:36:55 -0600

    Add boilerplate to store.js

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+
+    },
+    reducers: {
+
+    }
+})
+
+const store = configureStore({
+    reducer: appSlice.reducer
+})
+
+export default store;
