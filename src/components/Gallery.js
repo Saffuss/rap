e5e44122816f09130a2c0ae332987f69e5b534af
Smith Fusselman <smithfuss@gmail.com>
2024-09-26 21:28:20 -0600

    Add PopUp pop up functionality, as well as css styles for PopUp and a couple others

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,16 @@
 import React from "react";
 import '../App.css';
 
-function Gallery({ images }) {
+function Gallery({ images, togglePopUp }) {
     return (
         <div>
             <h2>Gallery</h2>
             <div className="images">
-                {images.map((image, index) => <a href={image} target="_blank" rel="noopener noreferrer" className="image-wrapper"><img src={image} key={index} alt="Space"/></a>)}
+                {images.map((image, index) => 
+                <div onClick={togglePopUp} key={index} className="image-wrapper">
+                    <img src={image} alt="Space"/>
+                </div>
+                )}
             </div>
         </div>
     )
--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Comments from "./Comments";
 
-function PopUp() {
+function PopUp({ showPopUp, togglePopUp }) {
+    const popUpClassName = showPopUp ? "pop-up pop-up-true" : "pop-up pop-up-false";
+
     return (
-        <div>
+        <div className={popUpClassName} onClick={togglePopUp}>
             <h2>PopUp</h2>
             <Comments />
         </div>
