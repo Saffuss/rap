10dc92d033fa7e19b5a7b69836916579cebef5db
Smith Fusselman <smithfuss@gmail.com>
2024-09-27 14:05:17 -0600

    Linked the search and filter state with the gallery to only show images that match the search. Supplemental css

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,19 +3,31 @@ import '../App.css';
 import { useSelector, useDispatch } from "react-redux";
 import { show } from "../store/store";
 
-function Gallery({ images, togglePopUp }) {
+function Gallery({ images }) {
     const dispatch = useDispatch();
+    const search = useSelector(state => state.filter.query);
+    const filters = useSelector(state => state.filter.filter);
+
+    const query = search + filters;
+
+    let filteredImages
+    if (query === '') {
+        filteredImages = images;
+    } else {
+        filteredImages = images.filter(image => image.toLowerCase().includes(query.toLowerCase()))
+    }
 
     return (
         <div>
             <h2>Gallery</h2>
             <div className="images">
-                {images.map((image, index) => 
+                {filteredImages.map((image, index) => 
                 <div onClick={() => dispatch(show(image))} key={index} className="image-wrapper">
                     <img src={image} alt="Space"/>
                 </div>
                 )}
             </div>
+            {filteredImages.length < 1 ? (<p>No images matched your search</p>) : null}
         </div>
     )
 }
